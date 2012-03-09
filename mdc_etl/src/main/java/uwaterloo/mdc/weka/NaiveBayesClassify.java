aa412c7bc7f99ef8707d543f738634ace2cf0a97
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-03-09 14:18:08 -0500

    Feature selection

--- mdc_etl/src/main/java/uwaterloo/mdc/weka/NaiveBayesClassify.java
+++ mdc_etl/src/main/java/uwaterloo/mdc/weka/NaiveBayesClassify.java
@@ -19,14 +19,18 @@ import org.apache.commons.math.stat.Frequency;
 import org.apache.commons.math.stat.descriptive.SummaryStatistics;
 
 import uwaterloo.mdc.etl.Config;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GreedyStepwise;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.WekaException;
 import weka.core.converters.ArffLoader;
 
 public class NaiveBayesClassify implements Callable<Void> {
 
-	private class FoldCallable implements Callable<Double> {
+	private class FoldCallable implements Callable<Double[]> {
 
 		final int v;
 
@@ -35,14 +39,21 @@ public class NaiveBayesClassify implements Callable<Void> {
 		}
 
 		@Override
-		public Double call() throws Exception {
-			double accuracy = 0;
-			Frequency[] foldConfusionMatrix;
+		public Double[] call() throws Exception {
+			Double[] accuracy = {0.0,0.0};
 
+			Frequency[] foldConfusionMatrix;
 			foldConfusionMatrix = new Frequency[Config.NUM_LABELS_CONSIDERED];
 			for (int i = 0; i < foldConfusionMatrix.length; ++i) {
 				foldConfusionMatrix[i] = new Frequency();
 			}
+
+			Frequency[] foldFeactSelectCM;
+			foldFeactSelectCM = new Frequency[Config.NUM_LABELS_CONSIDERED];
+			for (int i = 0; i < foldFeactSelectCM.length; ++i) {
+				foldFeactSelectCM[i] = new Frequency();
+			}
+
 			int foldStart = v * Config.VALIDATION_FOLD_WIDTH;
 
 			Collection<File> inputArrfs = FileUtils.listFiles(
@@ -60,20 +71,21 @@ public class NaiveBayesClassify implements Callable<Void> {
 					validationSet = new Instances(Channels.newReader(FileUtils
 							.openInputStream(userData).getChannel(),
 							Config.OUT_CHARSET));
-					validationSet.setClassIndex(validationSet.numAttributes() - 1);
+					validationSet
+							.setClassIndex(validationSet.numAttributes() - 1);
 				} else {
-					
+
 					ArffLoader dataLoader = new ArffLoader();
 					dataLoader.setFile(userData);
-					
+
 					// load structure
 					Instances structure = dataLoader.getStructure();
 					structure.setClassIndex(structure.numAttributes() - 1);
-					
+
 					if (firstUser) {
 						nb.buildClassifier(structure);
 					}
-					
+
 					// load data
 					Instance current;
 					while ((current = dataLoader.getNextInstance(structure)) != null) {
@@ -102,24 +114,25 @@ public class NaiveBayesClassify implements Callable<Void> {
 				classificationsWr
 						.append("instance\tclass1Prob\tclass2Prob\tclass3Prob\tclass4Prob\tclass5Prob\tclass6Prob\tclass7Prob\tclass8Prob\tclass9Prob\tclass10Prob\n");
 				if (validationSet.numInstances() == 0) {
-					classificationsWr.append("Not validation data for fold: "
+					classificationsWr.append("No validation data for fold: "
 							+ v);
 
 				} else {
 					int trueClassificationsCount = 0;
-					
+
 					for (int i = 0; i < validationSet.numInstances(); ++i) {
 						Instance vInst = validationSet.instance(i);
-						
-						if(vInst.classIsMissing()){
+
+						if (vInst.classIsMissing()) {
 							continue;
 						}
-						
-						Instance classMissing = (Instance)vInst.copy();
-					    classMissing.setDataset(vInst.dataset());
-					    classMissing.setClassMissing();
-					    
-						double[] vClassDist = nb.distributionForInstance(classMissing);
+
+						Instance classMissing = (Instance) vInst.copy();
+						classMissing.setDataset(vInst.dataset());
+						classMissing.setClassMissing();
+
+						double[] vClassDist = nb
+								.distributionForInstance(classMissing);
 						classificationsWr.append(vInst.dataset().relationName()
 								+ "[" + i + "]");
 						double vClassMaxProb = Double.NEGATIVE_INFINITY;
@@ -131,24 +144,25 @@ public class NaiveBayesClassify implements Callable<Void> {
 								vClass = j + 1;
 							}
 						}
+						classificationsWr.append('\n');
 						// The class "Value" is actually its index!!!!!!
 						if (vClass == vInst.classValue() + 1) {
 							++trueClassificationsCount;
 						}
 						long trueLabelCfMIx = Math.round(vInst.classValue());
 						long bestLabelInt = Math.round(vClass);
-						foldConfusionMatrix[(int) trueLabelCfMIx]
+						foldFeactSelectCM[(int) trueLabelCfMIx]
 								.addValue(bestLabelInt);
 						synchronized (totalConfusionMatrix) {
 							totalConfusionMatrix[(int) trueLabelCfMIx]
 									.addValue(bestLabelInt);
 						}
 					}
-					accuracy = trueClassificationsCount * 1.0
+					accuracy[0] = trueClassificationsCount * 1.0
 							/ validationSet.numInstances();
-					synchronized (cvAccuracyWr) {
+					synchronized (cvClassificationAccuracyWr) {
 
-						cvAccuracyWr
+						cvClassificationAccuracyWr
 								.append(Integer.toString(v))
 								.append('\t')
 								.append(Integer
@@ -156,7 +170,7 @@ public class NaiveBayesClassify implements Callable<Void> {
 								.append('\t')
 								.append(Integer.toString(validationSet
 										.numInstances())).append('\t')
-								.append(Double.toString(accuracy)).append('\n');
+								.append(Double.toString(accuracy[0])).append('\n');
 					}
 
 				}
@@ -170,13 +184,112 @@ public class NaiveBayesClassify implements Callable<Void> {
 									+ "_naive-bayes_confusion-matrix.txt"))
 							.getChannel(), Config.OUT_CHARSET);
 			try {
-				writeConfusionMatrix(foldConfusionWr, foldConfusionMatrix);
+				writeConfusionMatrix(foldConfusionWr, foldFeactSelectCM);
 			} finally {
 				foldConfusionWr.flush();
 				foldConfusionWr.close();
 			}
 			System.out.println((System.currentTimeMillis() - startTime)
 					+ " (fold " + v + "): Finished validation for fold: " + v);
+
+			// //////////////////////////////////////
+
+			AttributeSelectedClassifier featSelector = new AttributeSelectedClassifier();
+			CfsSubsetEval eval = new CfsSubsetEval();
+			GreedyStepwise search = new GreedyStepwise();
+			search.setSearchBackwards(true);
+
+			featSelector.setClassifier(nb);
+			featSelector.setEvaluator(eval);
+			featSelector.setSearch(search);
+
+			Writer featSelectWr = Channels.newWriter(
+					FileUtils.openOutputStream(
+							FileUtils.getFile(outputPath, "v" + v
+									+ "_naive-bayes_feat-selected-classifications.txt"))
+							.getChannel(), Config.OUT_CHARSET);
+			try {
+				featSelector.buildClassifier(validationSet);
+
+				featSelectWr
+						.append("instance\tclass1Prob\tclass2Prob\tclass3Prob\tclass4Prob\tclass5Prob\tclass6Prob\tclass7Prob\tclass8Prob\tclass9Prob\tclass10Prob\n");
+				if (validationSet.numInstances() == 0) {
+					featSelectWr.append("Not validation data for fold: " + v);
+
+				} else {
+					int featSelectCorrectCount = 0;
+
+					for (int i = 0; i < validationSet.numInstances(); ++i) {
+						Instance vInst = validationSet.instance(i);
+
+						if (vInst.classIsMissing()) {
+							continue;
+						}
+
+						Instance classMissing = (Instance) vInst.copy();
+						classMissing.setDataset(vInst.dataset());
+						classMissing.setClassMissing();
+
+						double[] vClassDist = featSelector
+								.distributionForInstance(classMissing);
+						featSelectWr.append(vInst.dataset().relationName()
+								+ "[" + i + "]");
+						double vClassMaxProb = Double.NEGATIVE_INFINITY;
+						double vClass = -1;
+						for (int j = 0; j < vClassDist.length; ++j) {
+							featSelectWr.append("\t" + vClassDist[j]);
+							if (vClassDist[j] > vClassMaxProb) {
+								vClassMaxProb = vClassDist[j];
+								vClass = j + 1;
+							}
+						}
+						featSelectWr.append('\n');
+						// The class "Value" is actually its index!!!!!!
+						if (vClass == vInst.classValue() + 1) {
+							++featSelectCorrectCount;
+						}
+						long trueLabelCfMIx = Math.round(vInst.classValue());
+						long bestLabelInt = Math.round(vClass);
+						foldFeactSelectCM[(int) trueLabelCfMIx]
+								.addValue(bestLabelInt);
+						synchronized (totalFeatSelectCM) {
+							totalFeatSelectCM[(int) trueLabelCfMIx]
+									.addValue(bestLabelInt);
+						}
+					}
+					accuracy[1] = featSelectCorrectCount * 1.0
+							/ validationSet.numInstances();
+					synchronized (cvFeatSelectAccuracyWr) {
+
+						cvFeatSelectAccuracyWr
+								.append(Integer.toString(v))
+								.append('\t')
+								.append(Integer
+										.toString(featSelectCorrectCount))
+								.append('\t')
+								.append(Integer.toString(validationSet
+										.numInstances())).append('\t')
+								.append(Double.toString(accuracy[1])).append('\n');
+					}
+
+				}
+			} catch (WekaException e) {
+				if (e.getMessage().startsWith("Not enough training instances")) {
+					featSelectWr.append(e.getMessage());
+				}
+			} finally {
+				featSelectWr.flush();
+				featSelectWr.close();
+			}
+
+			FileUtils.writeStringToFile(FileUtils.getFile(outputPath, "v" + v
+									+ "_naive-bayes_feat-selection.txt"), featSelector.toString());
+			
+			System.out.println((System.currentTimeMillis() - startTime)
+					+ " (fold " + v
+					+ "): Finished feature selection for fold: " + v);
+			// //////////////////////////////////////
+
 			return accuracy;
 		}
 
@@ -186,23 +299,34 @@ public class NaiveBayesClassify implements Callable<Void> {
 	private String inPath = "C:\\mdc-datasets\\weka\\segmented_user";
 
 	private final Frequency[] totalConfusionMatrix;
+	private final Frequency[] totalFeatSelectCM;
 	private long startTime = System.currentTimeMillis();
 
 	private final int inFoldTestIx = new Random(System.currentTimeMillis())
 			.nextInt(Config.VALIDATION_FOLD_WIDTH);
 
-	private final Writer cvAccuracyWr;
+	private final Writer cvClassificationAccuracyWr;
+	private final Writer cvFeatSelectAccuracyWr;
 
 	public NaiveBayesClassify() throws IOException {
 		totalConfusionMatrix = new Frequency[Config.NUM_LABELS_CONSIDERED];
 		for (int i = 0; i < totalConfusionMatrix.length; ++i) {
 			totalConfusionMatrix[i] = new Frequency();
 		}
-		cvAccuracyWr = Channels.newWriter(
+		totalFeatSelectCM = new Frequency[Config.NUM_LABELS_CONSIDERED];
+		for (int i = 0; i < totalFeatSelectCM.length; ++i) {
+			totalFeatSelectCM[i] = new Frequency();
+		}
+		cvClassificationAccuracyWr = Channels.newWriter(
 				FileUtils.openOutputStream(
 						FileUtils.getFile(outputPath,
-								"naive-bayes_accuracy-cv.txt")).getChannel(),
-				Config.OUT_CHARSET);
+								"naive-bayes_classification-accuracy-cv.txt"))
+						.getChannel(), Config.OUT_CHARSET);
+		cvFeatSelectAccuracyWr = Channels.newWriter(
+				FileUtils.openOutputStream(
+						FileUtils.getFile(outputPath,
+								"naive-bayes_feat-selected-accuracy-cv.txt"))
+						.getChannel(), Config.OUT_CHARSET);
 	}
 
 	/**
@@ -229,10 +353,15 @@ public class NaiveBayesClassify implements Callable<Void> {
 
 	public void acrossUsersClassify() throws Exception {
 
-		SummaryStatistics accuracySummary = new SummaryStatistics();
+		SummaryStatistics accuracySummaryAllFeatures = new SummaryStatistics();
+		SummaryStatistics accuracySummaryFeatSelected = new SummaryStatistics();
 
-		cvAccuracyWr.append("vFold").append('\t').append("trueN").append('\t')
-				.append("totalN").append('\t').append("accuracy").append('\n');
+		cvClassificationAccuracyWr.append("vFold").append('\t').append("trueN")
+				.append('\t').append("totalN").append('\t').append("accuracy")
+				.append('\n');
+		cvFeatSelectAccuracyWr.append("vFold").append('\t').append("trueN")
+				.append('\t').append("totalN").append('\t').append("accuracy")
+				.append('\n');
 		try {
 
 			System.out.println(new Date().toString()
@@ -241,7 +370,7 @@ public class NaiveBayesClassify implements Callable<Void> {
 					+ Config.VALIDATION_FOLD_WIDTH + " users");
 			ExecutorService foldExecutors = Executors
 					.newFixedThreadPool(Config.NUM_THREADS);
-			ArrayList<Future<Double>> foldFutures = new ArrayList<Future<Double>>();
+			ArrayList<Future<Double[]>> foldFutures = new ArrayList<Future<Double[]>>();
 			int numClassifyTasks = 0;
 			for (int v = 0; v < Config.VALIDATION_FOLDS; ++v) {
 				FoldCallable FoldCallable = new FoldCallable(v);
@@ -249,7 +378,9 @@ public class NaiveBayesClassify implements Callable<Void> {
 				++numClassifyTasks;
 			}
 			for (int i = 0; i < numClassifyTasks; ++i) {
-				accuracySummary.addValue(foldFutures.get(i).get());
+				Double[] accuracies = foldFutures.get(i).get();
+				accuracySummaryAllFeatures.addValue(accuracies[0]);
+				accuracySummaryFeatSelected.addValue(accuracies[1]);
 			}
 			foldExecutors.shutdown();
 			while (!foldExecutors.isTerminated()) {
@@ -258,13 +389,17 @@ public class NaiveBayesClassify implements Callable<Void> {
 				Thread.sleep(1000);
 			}
 		} finally {
-			cvAccuracyWr.flush();
-			cvAccuracyWr.close();
+			cvClassificationAccuracyWr.flush();
+			cvClassificationAccuracyWr.close();
+			cvFeatSelectAccuracyWr.flush();
+			cvFeatSelectAccuracyWr.close();
 		}
-		FileUtils
-				.writeStringToFile(FileUtils.getFile(outputPath,
-						"naive-bayes_accuracy-summary.txt"), accuracySummary
-						.toString());
+		FileUtils.writeStringToFile(FileUtils.getFile(outputPath,
+				"naive-bayes_accuracy-summary.txt"), accuracySummaryAllFeatures
+				.toString());
+		FileUtils.writeStringToFile(FileUtils.getFile(outputPath,
+				"naive-bayes_feat-selected_accuracy-summary.txt"),
+				accuracySummaryFeatSelected.toString());
 
 		Writer totalConfusionWr = Channels.newWriter(
 				FileUtils.openOutputStream(
@@ -276,6 +411,21 @@ public class NaiveBayesClassify implements Callable<Void> {
 		} finally {
 			totalConfusionWr.flush();
 			totalConfusionWr.close();
+		}
+
+		Writer featSelectedConfusionWr = Channels
+				.newWriter(
+						FileUtils
+								.openOutputStream(
+										FileUtils
+												.getFile(outputPath,
+														"naive-bayes_feat-selected-confusion-matrix.txt"))
+								.getChannel(), Config.OUT_CHARSET);
+		try {
+			writeConfusionMatrix(featSelectedConfusionWr, totalFeatSelectCM);
+		} finally {
+			featSelectedConfusionWr.flush();
+			featSelectedConfusionWr.close();
 		}
 
 		System.out.println(new Date().toString() + " (total): Done in "
